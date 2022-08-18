eb4b4b6983ed4ec8c9914c0adb532100015c5507
khoahoang1-admin <hoangdangkhoa0797@gmail.com>
2022-08-18 09:00:24 +0700

    m

--- playbook/menu.sh
+++ playbook/menu.sh
@@ -2,17 +2,46 @@
 #!/bin/bash
 # Bash Menu Script Example
 
+function installansible() {
+	{
+        apt install python3-pip -y
+        pip3 install ansible
+        mkdir -p /etc/ansible/
+        printf '
+        ansible ansible_host=192.168.253.140 ansible_port=22 ansible_user=root
+
+        [reachable]
+        ubuntu202 ansible_host=192.168.253.139 ansible_port=22 ansible_user=root
+
+        [worker]
+        ubuntu201 ansible_host=192.168.253.138 ansible_port=22 ansible_user=root
+        ' > /etc/ansible/hosts
+	}
+}
+function copykeygen()
+{   
+    sudo apt-get install -y sshpass
+    for ip in `cat server.txt`; do
+        ssh-copy-id -i ~/.ssh/id_rsa.pub -p 22 $ip
+    done
+}
+
 PS3='Please enter your choice: '
-options=("install ansible" "update file host" "install manager leader" "install manager reachable" "install worker" "Quit")
+options=("install ansible" "copy keygen ssh" "update file host" "install manager leader" "install manager reachable" "install worker" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
         "install ansible")
-            chmod a+x installansible.sh
-            /root/installansible.sh
+            installansible
+            ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N "" 0>&-
+            ;;
+        "copy keygen ssh")
+            nano server.txt
+            copykeygen
             ;;
         "update file host")
-            cp -f /root/hosts /etc/ansible/hosts
+            nano hosts
+            cp -f ./hosts /etc/ansible/hosts
             ;;
         "install manager leader")
             ansible-playbook umanager.yml
