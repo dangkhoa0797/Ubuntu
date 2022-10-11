b99f03f8817d5bc42da6e98d1b7c1b47e06b33f3
khoahd <hoangdangkhoa0797@gmail.com>
2022-10-11 09:10:58 +0700

    k

--- openSSHufw.sh
+++ openSSHufw.sh
@@ -4,6 +4,7 @@ sudo ufw default allow outgoing
 sudo ufw allow OpenSSH
 #sudo ufw allow 80
 #sudo ufw allow 443
+#sudo ufw allow 21/tcp
 sudo ufw --force enable
 #yes | sudo ufw enable
 #echo "y" | sudo ufw enable
